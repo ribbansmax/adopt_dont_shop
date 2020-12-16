17aa6ab55a140f48fc4efc048793565975b64588
Max Ribbans <ribbans.max@gmail.com>
2020-12-16 16:18:00 -0600

    Add approved to adoption

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_175859) do
+ActiveRecord::Schema.define(version: 2020_12_16_221712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_12_175859) do
     t.bigint "pet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved"
     t.index ["application_id"], name: "index_adoptions_on_application_id"
     t.index ["pet_id"], name: "index_adoptions_on_pet_id"
   end
