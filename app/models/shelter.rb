7459c9d7393da7b5a2a5b40fb98c462cff7b6b83
Max Ribbans <ribbans.max@gmail.com>
2020-12-16 20:55:16 -0600

    Add Shelter.pending method to find all shelters with pending apps

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,10 @@
 class Shelter < ApplicationRecord
   has_many :pets
+
+  def self.pending
+    pending_apps = Application.where(completed: true, approved: nil).select(:id)
+    pending_pets = Adoption.where(application_id: pending_apps).select(:pet_id)
+    shelter_ids = Pet.where(id: pending_pets).select(:shelter_id)
+    Shelter.where(id: shelter_ids)
+  end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -4,4 +4,25 @@ describe Shelter, type: :model do
   describe 'relationships' do
     it { should have_many :pets }
   end
+
+  it 'can list all shelters with pending applications' do
+    @shelter1 = Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+    @shelter2 = Shelter.create!(name: "Silly Shelter", address: "123 Silly Ave", city: "Longmont", state: "CO", zip: 80012)
+    @shelter3 = Shelter.create!(name: "Shell Shelter", address: "102 Shelter Dr.", city: "Commerce City", state: "CO", zip: 80022)
+
+    @pet1 = @shelter1.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+    @pet2 = @shelter2.pets.create!(image:"", name: "Athena", description: "cat", approximate_age: 3, sex: "female")
+    @pet3 = @shelter1.pets.create!(image:"", name: "Zeus", description: "dog", approximate_age: 4, sex: "male")
+
+    @app1 = Application.create!(name: "Dunlap", address: "104 Pine Haven", city: "Colchester", state: "VT", zip: "05446")
+    @app2 = Application.create!(name: "Karen", address: "15 Quincy", city: "Somerville", state: "MA", zip: "02143")
+
+    @a1 = Adoption.create!(pet_id: @pet1.id, application_id: @app1.id)
+    @a2 = Adoption.create!(pet_id: @pet2.id, application_id: @app1.id)
+
+    @app1.update!(completed: true)
+    @app2.update!(completed: true)
+
+    expect(Shelter.pending).to eq([@shelter1, @shelter2])
+  end
 end
